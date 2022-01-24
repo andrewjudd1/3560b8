6c06c4fc9f63096791fa5db4a53b4ce706f9f130
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-24 01:20:30 -0700

    connecting db

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+// const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
+//   logging: false
+// });
+const db = new Sequelize('messenger', process.env.PSQL_USER, process.env.PSQL_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
 module.exports = db;
