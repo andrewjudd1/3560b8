d121a053eb9ee8e4b458f46ad29679688ed61e71
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-24 18:13:56 -0700

    trying wrong password

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 // const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
 //   logging: false
 // });
-const db = new Sequelize('messenger', `postgres`, `Dwylmlomdns15&`, {
+const db = new Sequelize('messenger', `postgres`, `wylmlomdns15&`, {
   host: 'localhost',
   dialect: 'postgres'
 });
