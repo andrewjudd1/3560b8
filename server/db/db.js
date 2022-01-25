bea638297f26ff90cf39a99ad43085cb66619464
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-24 18:38:04 -0700

    changing database name

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 // const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
 //   logging: false
 // });
-const db = new Sequelize('messenger', `postgres`, `now3ef239vlkn`, {
+const db = new Sequelize('postgres', `postgres`, `now3ef239vlkn`, {
   host: 'localhost',
   dialect: 'postgres'
 });
