b62a26af90cfc5f3163e74e741cdc64f90390b35
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-24 20:10:14 -0700

    changing back to local variables

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,11 @@ const Sequelize = require("sequelize");
 // const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
 //   logging: false
 // });
-const db = new Sequelize('postgres', `postgres`, `now3ef239vlkn`, {
+// const db = new Sequelize('postgres', `postgres`, `now3ef239vlkn`, {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+const db = new Sequelize('messenger', `${process.env.PSQL_USER}`, `${process.env.PSQL_PASSWORD}`, {
   host: 'localhost',
   dialect: 'postgres'
 });
