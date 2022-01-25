bd62aa28d04bae2f3f57dd265e78bcfb8f70a714
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-24 17:49:16 -0700

    changing to username and password to .env

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const Sequelize = require("sequelize");
 
 // const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
 //   logging: false
 // });
-const db = new Sequelize('messenger', 'postgres', 'Dwylmlomdns15&', {
+const db = new Sequelize('messenger', `${process.env.PSQL_USER}`, `${process.env.PSQL_PASSWORD}`, {
   host: 'localhost',
   dialect: 'postgres'
 });
