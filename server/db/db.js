bf4ab4dbab3ff71d26528e86ba901282a3f75849
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-24 17:31:09 -0700

    trying run seed again

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 // const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
 //   logging: false
 // });
-const db = new Sequelize('messenger', `postgres`, `Dwylmlomdns15&`, {
+const db = new Sequelize('messenger', 'postgres', 'Dwylmlomdns15&', {
   host: 'localhost',
   dialect: 'postgres'
 });
