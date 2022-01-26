192d996ef5c637a11384a161eabccf22deb502da
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-25 20:40:06 -0700

    fixing run seed bug

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || 'messenger', `${process.env.PSQL_USER}`, `${process.env.PSQL_PASSWORD}`, {
+
+const db = new Sequelize('postgres', 'postgres', "now3ef239vlkn", {
   host: 'localhost',
   dialect: 'postgres'
-});
+})
+
+// const db = new Sequelize('messenger', `${process.env.PSQL_USER}`, `${process.env.PSQL_PASSWORD}`, {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
 
 module.exports = db;
