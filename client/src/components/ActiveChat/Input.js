92708fef7ff2a2978de15719026170ccab0d4585
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-27 03:48:53 -0700

    adding feature: sending images on the frontend

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import {FilledInput, InputLabel, Box, Typography, Button} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import CropOriginalIcon from '@material-ui/icons/CropOriginal';
+import Delete from '@material-ui/icons/Delete';
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
 
@@ -9,22 +11,94 @@ const useStyles = makeStyles(() => ({
     justifySelf: "flex-end",
     marginTop: 15
   },
-  input: {
+  
+  ImageInputContainer: {
+    position: 'relative',
+    marginBottom: 20,
     height: 70,
-    backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
+    backgroundColor: "#F4F6FA",
+
+
+  },
+ 
+  Input: {
+    position: 'absolute',
+    width: '100%',
+    height: 70,
+    paddingBottom: 15,
+    borderRadius: 8,
+    backgroundColor: "#F4F6FA",
+
+  },
+  ImageInputLabel: {
+    position: 'absolute',
+    right: '10px',
+    top: 'calc(50% - 8px)',
+    cursor: 'pointer',
+  },
+  ImageInput: { 
+    display: 'none'
+  },
+  ImageUploadContainer: {
+    display: 'flex',
+    width: '100%',
+    background: '#F4F6FA',
+  },
+  ImageUploadTopContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  ImageUpload: {
+    width: '200px',
   }
 }));
 
 const Input = (props) => {
+  const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/andrew-judd/image/upload'
+  const CLOUDINARY_UPLOAD_PRESET = 'oxzlmelj'
   const classes = useStyles();
-  const [text, setText] = useState("");
+  const [text, setText] = useState("")
+  const [images, setImages] = useState([]); 
   const { postMessage, otherUser, conversationId, user, refresh} = props;
+  
   const handleChange = (event) => {
     setText(event.target.value);
   };
 
+  const uploadImage = (e) => {
+    const file = e.target.files[0]
+    console.log(file)
+    const formData = new FormData();
+    formData.set('file', file);
+    formData.set('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+    console.log(formData.get('upload_preset'))
+
+    fetch(CLOUDINARY_URL, {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.secure_url !== '') {
+        
+        const uploadedFileUrl = data.secure_url
+        console.log(uploadedFileUrl)
+        let newImage = uploadedFileUrl
+        console.log(newImage)
+        
+        setImages([...images, newImage])
+      }
+      
+    }).catch(err => console.log(err))
+
+  }
+  const deleteImage = (images, index) => {
+   const newArray = images.filter(image=> images.indexOf(image) !== index)
+   setImages(newArray)
+   
+  }
   const handleSubmit = async (event) => {
     event.preventDefault();
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
@@ -32,25 +106,49 @@ const Input = (props) => {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
+      attachments: images
     };
     await postMessage(reqBody);
     setText("");
+    setImages([])
     refresh()
   };
 
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
-      <FormControl fullWidth hiddenLabel>
+      <Box className={classes.ImageUploadContainer}>
+        {images.map((image, index) => 
+          <Box key={index}>
+              <Box className={classes.ImageUploadTopContainer}>
+              <Typography>{`Image${index + 1}:`}
+              </Typography>
+              <Button onClick={() => deleteImage(images, index)}><Delete/></Button>
+              </Box>
+              <img className={classes.ImageUpload} alt={'upload'} src={image}>
+              </img>
+          </Box>)}
+      </Box>
+      <Box className={classes.ImageInputContainer}>
         <FilledInput
-          classes={{ root: classes.input }}
+          className={classes.Input}
           disableUnderline
           placeholder="Type something..."
           value={text}
           name="text"
           onChange={handleChange}
-        />
-      </FormControl>
+        >
+        </FilledInput>
+        <InputLabel className={classes.ImageInputLabel} htmlFor='imageInput'>
+          <CropOriginalIcon/>
+        </InputLabel>
+      </Box>
+      <FilledInput
+        className={classes.ImageInput}
+        id='imageInput'
+        onChange={uploadImage}
+        type="file">
+      </FilledInput>
     </form>
   );
 };
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography,} from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "flex-end"
+    alignItems: "flex-end",
+    margin: '15px 0',
   },
   date: {
+    order: '1',
+    fontSize: 11,
+    color: "#BECCE2",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  OneImageDate: {
+    order: '0',
     fontSize: 11,
     color: "#BECCE2",
     fontWeight: "bold",
@@ -19,24 +28,88 @@ const useStyles = makeStyles(() => ({
     color: "#91A3C0",
     letterSpacing: -0.2,
     padding: 8,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   bubble: {
     background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
+    borderRadius: "10px 10px 0 10px",
+    
+  },
+  OneImageBubble: {
+    order: '1',
+    background: "#F4F6FA",
+    borderRadius: "10px 10px 0 10px",
+  },
+  ImageList: {
+    display: 'flex',
+    gap: '5px',
+  },
+  ImageListItem: {
+    display: 'flex',
+    position: 'relative',
+  },
+  Image: {
+    width: '100px',
+    objectFit: 'contain',
+    cursor: 'pointer',
+    borderRadius: '15px',
+    border: '1px solid transparent',
+  },
+  OneImageListItem: {
+
+  },
+  OneImage: {
+    width: '200px',
+    objectFit: 'contain',
+    cursor: 'pointer',
+    borderRadius: '15px',
+    border: '1px solid transparent',
   }
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { time, text, attachments } = props;
   return (
     <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
-      <Box className={classes.bubble}>
-        <Typography className={classes.text}>{text}</Typography>
+      <Typography 
+        className={attachments?.length > 1 ? 
+        classes.date :
+        classes.OneImageDate}>
+        {time}
+      </Typography>
+      <Box 
+        className={attachments?.length > 1 ? 
+        classes.bubble : 
+        classes.OneImageBubble}>
+        <Typography className={classes.text}>
+          {text}
+        </Typography>
+      </Box>
+      <Box className={classes.ImageList}> {
+        attachments?.length > 1 ?
+          attachments.map((attachment, index) => (  
+            <Box 
+              key={index} 
+              className={classes.ImageListItem} >
+                <img
+                  onClick={() => window.open(attachment)}
+                  className={classes.Image}
+                  src={attachment}
+                  alt={index}/> 
+              </Box>)) : 
+        attachments?.length > 0 &&
+          <Box 
+            className={classes.OneImageListItem} >
+            <img
+              onClick={() => window.open(attachments[0])}
+              className={classes.OneImage}
+              src={attachments[0]}
+              alt={'one'}/> 
+          </Box> }
       </Box>
     </Box>
+  
   );
 };
 
