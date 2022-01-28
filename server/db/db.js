df6428ac4b9458ff0017b3fb3ec10ea8c2a97388
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-28 04:41:07 -0700

    db local setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
-  logging: false
+// const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
+//   logging: false
+// });
+// const db = new Sequelize('postgres', `postgres`, `now3ef239vlkn`, {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+const db = new Sequelize(`${process.env.PSQL_DATABASE}`, `${process.env.PSQL_USER}`, `${process.env.PSQL_PASSWORD}`, {
+  host: `${process.env.PSQL_HOST}`,
+  dialect: 'postgres'
 });
 
 module.exports = db;
