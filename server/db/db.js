497caf488e6745cf5d0b8f0ef62dce75403ce9e3
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-28 03:54:03 -0700

    fixing db credentials

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(`${process.env.PSQL_DATABASE}`, `${process.env.PSQL_USER}`, `${process.env.PSQL_PASSWORD}`, {
-  host: `${process.env.PSQL_HOST}`,
-  dialect: 'postgres'
+const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
+  logging: false
 });
 
-module.exports = db;
