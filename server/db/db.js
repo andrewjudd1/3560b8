8fcbe089a0b7cfbbcf4fdd9b9f6c7d1ef7ce2f0f
Andrew Judd <andrewmjudd1@gmail.com>
2022-01-28 21:47:16 -0700

    cleaning up code for merge

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,6 @@
-require('dotenv').config()
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres:localhost:null//localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 });
 
